8072fc6d4795982d31e422174829e108e088d831
devSviat <devsviat@proton.me>
2026-07-27 22:37:08 +0300

    refactor(vibe_shop): retire legacy stylesheets, extract vendor CSS

--- design/vibe_shop/css.php
+++ design/vibe_shop/css.php
@@ -13,11 +13,8 @@ use Okay\Core\TemplateConfig\Css;
 return [
     (new Css('tokens.css')),
     (new Css('grid.css')),
-    (new Css('okay.css')),
-    (new Css('theme.css')),
+    (new Css('vendor.css')),
     (new Css('select2.min.css')),
-    (new Css('media.css')),
-    (new Css('mobile_menu.css')),
     (new Css('base.css')),
     (new Css('components.css')),
 ];
