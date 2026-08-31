d583ebcca14f18bc3b78ea33e1c21ce57c25ab2a
devSviat <devsviat@proton.me>
2026-08-31 07:41:56 +0300

    chore: бамп forkVersion до 1.2.0

--- Okay/Core/Config.php
+++ Okay/Core/Config.php
@@ -16,7 +16,7 @@ class Config
     /*Версия системы*/
     public string $version = '4.5.2';
     /*Версія форку (лінійка релізів okaycms-fork/vX.Y.Z)*/
-    public string $forkVersion = '1.1.1';
+    public string $forkVersion = '1.2.0';
     /*Тип системы*/
     public string $version_type = 'pro';
 
